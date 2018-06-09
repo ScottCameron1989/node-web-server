a272608d02eac7b3fe8f482b3360a565b2b07b04
ScottCameron1989 <scottcameron1989@gmail.com>
2018-06-09 12:41:39 -0400

    finished pages with handlebars

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
 const express = require('express');
-
+const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n' , (err) => {
+    if (err) {
+      console.log('Unable to append server.log.')
+    }
+  });
+  next();
+});
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs')
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>
+{
+  return text.toUpperCase();
+});
 
 app.get('/',(req,res)=>{
   // res.send('<h1>hello Express!</h1>');
-  res.send({
-    name:'adrew',
-    like:['food','poutine']
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about',(req,res)=>{
-  res.send('about page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
 });
 
 app.get('/bad',(req,res)=>{
@@ -19,4 +51,6 @@ app.get('/bad',(req,res)=>{
     errorMessage: 'Unable to load request'
   });
 });
-app.listen(3000);
+app.listen(3000, ()=> {
+  console.log('Server is up on port 3000');
+});
